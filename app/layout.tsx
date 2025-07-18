22cf40a7a413a516c2cdf01257091bb24466e705
jangidRkt08 <itsmejangid.rk@gmail.com>
2025-07-18 17:29:01 +0530

    Created Startup Page/Validation (Zod)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 import 'easymde/dist/easymde.min.css';  
+import { Toaster } from "@/components/ui/toaster";
+
 
 const workSans = localFont({
   src: [
@@ -71,6 +73,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${workSans.variable}`}>
         {children}
+        <Toaster/>
       </body>
     </html>
   );
